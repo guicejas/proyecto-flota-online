c71e032326eab4aa52ef5b9742088ff39d91223a
guicejas <guicejas@gmail.com>
2014-08-03 19:33:19 -0300

    Agregando Perfiles

--- AppCursoAspNet/AppCursoAspNet/Seguridad/Perfiles.aspx.cs
+++ AppCursoAspNet/AppCursoAspNet/Seguridad/Perfiles.aspx.cs
@@ -9,9 +9,30 @@ namespace Vista.Seguridad
 {
     public partial class Perfiles : System.Web.UI.Page
     {
+        Controladora.SEGURIDAD.ControladoraPerfiles ctrlPerfiles = new Controladora.SEGURIDAD.ControladoraPerfiles();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public List<Modelo.SEGURIDAD.Perfil> GetPerfiles()
+        {
+            return ctrlPerfiles.ListarPerfiles();
+        }
+
+        protected void listaGrupos_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int perfilId = Convert.ToInt32(this.listaPerfiles.Rows[e.RowIndex].Cells[0].Text);
+            ctrlPerfiles.EliminarPerfil(perfilId);
+            e.Cancel = true;
+            this.listaPerfiles.DataSource = null;
+        }
+
+        protected void listaGrupos_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            int perfilId = Convert.ToInt32(this.listaPerfiles.Rows[e.NewEditIndex].Cells[0].Text);
+            Response.Redirect("EditarPerfil?PerfilId=" + perfilId);
         }
     }
 }
